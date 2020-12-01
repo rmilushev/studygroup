e3d5a782b34f778f742a599e3bc10c579792eb09
Yasen Kostov <qskatak04@gmail.com>
2020-12-01 19:07:55 +0200

    fix GЕT routes

--- lib/studygroup_web/router.ex
+++ lib/studygroup_web/router.ex
@@ -3,6 +3,9 @@ defmodule StudygroupWeb.Router do
 
   import StudygroupWeb.UserAuth
 
+  resources "/student", StudentController
+
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -66,6 +69,7 @@ defmodule StudygroupWeb.Router do
     put "/users/settings/update_password", UserSettingsController, :update_password
     put "/users/settings/update_email", UserSettingsController, :update_email
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    put "/student", StudentController, :create
   end
 
   scope "/", StudygroupWeb do
